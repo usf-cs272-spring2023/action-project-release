9832bb2685b38909cc42c5a87617b1bc8bb7a321
Sophie Engle <sjengle@cs.usfca.edu>
2022-08-24 20:35:37 -0700

    Uploading results

--- index.js
+++ index.js
@@ -6,28 +6,34 @@ module.exports = async ({github, context, core, fs}) => {
   out.project_repo = process.env.PROJECT_REPO;
   out.release_tag  = process.env.RELEASE_TAG;
 
+  core.info('');
   core.info(`Repository: ${out.project_repo}`);
   core.info(`   Release: ${out.release_tag}`);
-  core.info('');
 
   // parse release tag into parts
   const regex = /^v([1-4])\.(\d+)\.(\d+)$/;
   const matched = out.release_tag.match(regex);
 
   if (matched === null || matched.length !== 4) {
-    throw new Error(`Unable to parse "${release_tag}" into major, minor, and patch version numbers.`);
+    core.info('');
+    throw new Error(`Unable to parse "${out.release_tag}" into major, minor, and patch version numbers.`);
   }
 
   out.project_num = parseInt(matched[1]);
   out.review_num  = parseInt(matched[2]);
   out.patch_num   = parseInt(matched[3]);
 
+  core.info('');
   core.startGroup('Outputting results...');
   core.info(JSON.stringify(out));
   core.endGroup();
 
-  const filename = 'check-project-release.json';
+  const artifact_id = 'check-project-release';
+  const filename = `${artifact_id}.json`;
+
   fs.writeFileSync(filename, JSON.stringify(out));
+  core.setOutput('results_name', artifact_id);
+  core.setOutput('results_json', filename);
 
   return out;
 }
