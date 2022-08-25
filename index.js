33d027bc7deca7721f7797330db9c8085460cb67
Sophie Engle <sjengle@cs.usfca.edu>
2022-08-24 20:40:28 -0700

    Changing outputs

--- index.js
+++ index.js
@@ -19,21 +19,25 @@ module.exports = async ({github, context, core, fs}) => {
     throw new Error(`Unable to parse "${out.release_tag}" into major, minor, and patch version numbers.`);
   }
 
+  // save parsed values
   out.project_num = parseInt(matched[1]);
   out.review_num  = parseInt(matched[2]);
   out.patch_num   = parseInt(matched[3]);
 
+  // add artifact name and json file
+  out.results_name = 'check-project-release';
+  out.results_json = `${out.results_name}.json`;
+
+  // write results to file
+  fs.writeFileSync(out.results_json, JSON.stringify(out));
+  core.setOutput('results_name', out.results_name);
+  core.setOutput('results_json', out.results_json);
+
+  // display final results before returning
   core.info('');
   core.startGroup('Outputting results...');
   core.info(JSON.stringify(out));
   core.endGroup();
-
-  const artifact_id = 'check-project-release';
-  const filename = `${artifact_id}.json`;
-
-  fs.writeFileSync(filename, JSON.stringify(out));
-  core.setOutput('results_name', artifact_id);
-  core.setOutput('results_json', filename);
 
   return out;
 }
